b0d27e0f88a114cdcbcb452a61bf77c678709bbf
Soham Kolhatkar <sohamk130405@gmail.com>
2025-10-31 00:44:09 +0530

    multer dir fix

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,6 +1,12 @@
 // config/multer.js
+const fs = require("fs");
 const multer = require("multer");
 const path = require("path");
+
+const uploadDir = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
