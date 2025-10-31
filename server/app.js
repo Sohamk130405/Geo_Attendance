49263a7b939a2bb321e769b2d1ff3e65e0754039
Soham Kolhatkar <sohamk130405@gmail.com>
2025-10-31 12:51:58 +0530

    removed local ssl and ssh

--- server/app.js
+++ server/app.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const https = require("https");
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
 
 const authRoutes = require("./routes/authRoutes");
 const attendanceRoutes = require("./routes/attendanceRoutes");
@@ -23,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server running on Vercel 🚀" });
+  res.json({ message: "Server running on Render 🚀" });
 });
 
 app.get("/test", (req, res) => {
@@ -32,24 +28,6 @@ app.get("/test", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// Load SSL certificate and key
-const key = fs.readFileSync(
-  path.join(__dirname, "../client/geo_attendance-privateKey.key"),
-  "utf8"
-);
-const cert = fs.readFileSync(
-  path.join(__dirname, "../client/geo_attendance.crt"),
-  "utf8"
-);
-
-const httpsServer = https.createServer({ key, cert }, app);
-const httpServer = http.createServer(app);
-
-// Start both HTTP and HTTPS servers
-httpServer.listen(80, () => {
-  console.log("HTTP Server running on port 80");
-});
-
-httpsServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`HTTPS Server running on port ${PORT}`);
 });
